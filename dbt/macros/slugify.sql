3902f55296ba394bb9e5a3944464e24ab552a57d
Tim Hiebenthal <timhiebenthal@gmail.com>
2024-06-09 13:33:48 +0000

    intermediate progress

---
+++ dbt/macros/slugify.sql
@@ -0,0 +1,11 @@
+{% macro slugify_column_values(string) %}
+    regexp_replace(
+        regexp_replace(
+            regexp_replace(lower({{ string }}), '[ -]+', '_'),
+            '^[0-9]',
+            concat('_', substr({{ string }}, 1, 1))
+        ),
+        '[^a-z0-9_]+',
+        ''
+    )
+{% endmacro %}
