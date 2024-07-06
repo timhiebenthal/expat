7cc492a6b46f110c8f2682e94c99499f4b51c90f
Tim Hiebenthal <timhiebenthal@gmail.com>
2024-07-06 22:19:56 +0000

    make config editable

--- data_viz/streamlit_app.py
+++ data_viz/streamlit_app.py
@@ -3,7 +3,11 @@ import duckdb
 import os
 import altair as alt
 import utils
+import sys
+import pandas as pd
 from dbt.cli.main import dbtRunner, dbtRunnerResult
+
+config = utils.load_config()
 
 
 st.set_page_config(page_title="Cost of Living Analysis", layout="wide")
@@ -26,43 +30,57 @@ location_mapping = {
     "Rent 3 Room Outside of Center": "expense_3_roomappart_outside",
 }
 
+main_tab, config_tab = st.tabs(["Home", "Config"])
+
+
 # with st.popover("Configurations"):
 currency = st.sidebar.selectbox("Select currency:", ["EUR"])
 selected_job = st.sidebar.selectbox("Enter job title:", job_list)
 selected_cities = st.sidebar.multiselect("Select cities:", city_list, default=["Berlin", "Munich"])
 selected_location = st.sidebar.selectbox("Select location preference:", list(location_mapping.keys()))
 
+with main_tab:
+    st.markdown("*DISCLAIMER: The values are estimations and are solely for illustrative purposes.*")
 
-st.markdown("*DISCLAIMER: The values are estimations and are solely for illustrative purposes.*")
+    with st.container(border=True):
 
-with st.container(border=True):
-    selected_df = df.query("city_name.isin(@selected_cities) and job_title_experience_short == @selected_job", engine="python").assign(
-        rent_ratio=lambda x: x[location_mapping[selected_location]] / x["avg_monthly_net_salary_eur"]
-    )
-
-
-    salary_chart = utils.create_altair_labelled_vertical_bar_chart(selected_df, 'avg_monthly_net_salary_eur', 'Avg. Net Salary (EUR)', 'city_name', 'City')
-    rent_chart = utils.create_altair_labelled_vertical_bar_chart(selected_df, location_mapping[selected_location], 'Rent (EUR)', 'city_name', 'City')
-
-    columns = [
-        "city_name",
-        "job_title_experience_short",
-        "avg_monthly_net_salary_eur",
-        location_mapping[selected_location],
-        "rent_ratio"
-    ]
-
-    st.altair_chart(salary_chart | rent_chart, use_container_width=True)
-    st.dataframe(
-        data=selected_df[columns], #.rename(columns=df_column_mapping), 
-        hide_index=True,
-        use_container_width=True,
-        column_config={
-            "avg_monthly_net_salary_eur": st.column_config.NumberColumn("Avg. Net Salary (EUR)", format="€ %.0f"),
-            location_mapping[selected_location]: st.column_config.NumberColumn(selected_location, format="€ %.0f"),
-            "rent_ratio": st.column_config.ProgressColumn("Rent Ratio", min_value=0, max_value=1, format=" %.02f%%")
-            },
+        selected_df = df.query("city_name.isin(@selected_cities) and job_title_experience_short == @selected_job", engine="python").assign(
+            rent_ratio=lambda x: x[location_mapping[selected_location]] / x["avg_monthly_net_salary_eur"]
         )
 
+        salary_chart = utils.create_altair_labelled_vertical_bar_chart(selected_df, 'avg_monthly_net_salary_eur', 'Avg. Net Salary (EUR)', 'city_name', 'City')
+        rent_chart = utils.create_altair_labelled_vertical_bar_chart(selected_df, location_mapping[selected_location], 'Rent (EUR)', 'city_name', 'City')
 
-# /* to do: integrate dbt pipeline trigger; implement LLM for other jobs */
+        columns = [
+            "city_name",
+            "job_title_experience_short",
+            "avg_monthly_net_salary_eur",
+            location_mapping[selected_location],
+            "rent_ratio"
+        ]
+
+        st.altair_chart(salary_chart | rent_chart, use_container_width=True)
+        st.dataframe(
+            data=selected_df[columns], #.rename(columns=df_column_mapping), 
+            hide_index=True,
+            use_container_width=True,
+            column_config={
+                "avg_monthly_net_salary_eur": st.column_config.NumberColumn("Avg. Net Salary (EUR)", format="€ %.0f"),
+                location_mapping[selected_location]: st.column_config.NumberColumn(selected_location, format="€ %.0f"),
+                "rent_ratio": st.column_config.ProgressColumn("Rent Ratio", min_value=0, max_value=1, format=" %.02f%%")
+                },
+            )
+
+
+with config_tab:
+    st.text("Add cities or Jobs to the configuration file and update the pipeline afterwards to take changes into account.")
+    
+    with st.container(border=True):
+        cities = utils.editable_df_component(config, "cities")
+        jobs = utils.editable_df_component(config, "jobs")
+    
+    col1, col2, col3, col4 = st.columns(4)
+    col1.button("Save Changes", type="primary", on_click=utils.save_config, args=(cities, jobs), help="Overwrites the configuration file with the current values.")
+    col2.button("Update Pipeline", type="primary", on_click=utils.run_pipeline, help="Runs the data pipeline to take modifications into account.")
+
+
--- data_viz/utils.py
+++ data_viz/utils.py
@@ -1,4 +1,6 @@
 import altair as alt
+import streamlit as st
+
 
 def create_altair_labelled_vertical_bar_chart(df, x_field, x_label, y_field, y_label):
     """
@@ -19,3 +21,61 @@ def create_altair_labelled_vertical_bar_chart(df, x_field, x_label, y_field, y_l
         base.mark_bar()
         + base.mark_text(align='right', size=18, color='#FFFFFF', dx=-5)
         )
+
+
+def load_config():
+    import logging
+    import yaml
+    # configure timestamp for logging
+    logging.basicConfig(
+        format="%(asctime)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S", level=logging.INFO
+    )
+    # loads yaml-config and returns it as a dictionary
+    with open("config.yml", "r") as file:
+        logging.info("Loading cities from config.yml\n")
+        return yaml.safe_load(file)
+    
+
+def save_config(city_data, jobs_data):
+    import logging
+    import yaml
+    import time 
+    
+    # configure timestamp for logging
+    logging.basicConfig(
+        format="%(asctime)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S", level=logging.INFO
+    )
+    
+    config = {
+        "cities": city_data['cities'].dropna().tolist(),
+        "jobs": jobs_data['jobs'].dropna().tolist()
+    }
+
+    # saves the dataframe as a yaml file
+    with open("config.yml", "w") as file:
+        logging.info("Saving to config.yml\n")
+        yaml.dump(config, file)
+    
+    success_info = st.success("Config saved! :)", icon="✅")
+    time.sleep(2) # Wait for 3 seconds
+    success_info.empty() # Clear the alert
+
+
+def editable_df_component(config, object_name):
+    import pandas as pd
+
+    city_df = pd.DataFrame(data=config[object_name], columns=[object_name])
+    exp_cities = st.expander(object_name.title())
+
+    return exp_cities.data_editor(
+        data=city_df,
+        num_rows="dynamic", 
+        hide_index=True,
+        # height=750
+        )
+    
+
+
+
+def run_pipeline():
+    print("dummy run")
